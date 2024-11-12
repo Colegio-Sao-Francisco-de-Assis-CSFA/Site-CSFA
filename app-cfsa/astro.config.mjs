146b60b3813fa0927274c664492c91d08bee8cef
Cloves-CSFA <cvr.neto20@gmail.com>
2024-11-12 19:00:40 -0300

    feat: att design template

--- app-cfsa/astro.config.mjs
+++ app-cfsa/astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   output: 'server',
+
   adapter: netlify({
     edgeMiddleware: true
   }),
+
+  integrations: [icon()],
 });
